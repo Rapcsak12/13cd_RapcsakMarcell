a0d0f7867089f2dea34814a43855d5b2955bc871
Rapcsak12 <78409938+Rapcsak12@users.noreply.github.com>
2025-02-17 12:01:41 +0100

    navbar elkészítése

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {Link, NavLink} from "react-router-dom";
 
 const Navbar = (()=>{
 
     return(
         <div>
-            <h1>Menü</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero maxime dignissimos cumque voluptates eum unde officia dolore! Quod repudiandae provident exercitationem tempore odio accusantium necessitatibus quidem ipsam deleniti. Laborum quidem quod enim, maiores doloribus sequi amet rerum quia vitae voluptatem voluptas officiis voluptatum, ut delectus repudiandae atque repellendus eligendi hic? At neque voluptas eos aspernatur enim, fugit nulla fuga quidem rerum, accusantium sapiente quas, ratione magni cum soluta assumenda asperiores error. Temporibus asperiores incidunt consequatur ducimus quo placeat at facere, sint distinctio totam libero natus saepe assumenda quas esse nam voluptatum! Labore aperiam omnis dolor modi numquam ducimus quos!</p>
+            <nav>
+
+                <Link to='/'>Főoldal</Link>
+                <NavLink to='/ermesek'>Az olimpia érmesei</NavLink>
+                <NavLink to='/mermesek'>A magyar érmesek</NavLink>
+
+            </nav>
         </div>
     )
 
