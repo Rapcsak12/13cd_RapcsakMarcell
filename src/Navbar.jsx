8024f8ad7e73989fee94af573418c8e2efc20cf5
Rapcsak12 <78409938+Rapcsak12@users.noreply.github.com>
2025-02-17 11:29:53 +0100

    szükséges modulok telepítése

---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = (()=>{
+
+    return(
+        <div>
+            <h1>Menü</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit vero maxime dignissimos cumque voluptates eum unde officia dolore! Quod repudiandae provident exercitationem tempore odio accusantium necessitatibus quidem ipsam deleniti. Laborum quidem quod enim, maiores doloribus sequi amet rerum quia vitae voluptatem voluptas officiis voluptatum, ut delectus repudiandae atque repellendus eligendi hic? At neque voluptas eos aspernatur enim, fugit nulla fuga quidem rerum, accusantium sapiente quas, ratione magni cum soluta assumenda asperiores error. Temporibus asperiores incidunt consequatur ducimus quo placeat at facere, sint distinctio totam libero natus saepe assumenda quas esse nam voluptatum! Labore aperiam omnis dolor modi numquam ducimus quos!</p>
+        </div>
+    )
+
+}
+)
+export default Navbar
